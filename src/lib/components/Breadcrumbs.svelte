d39315855cd41d8bc61f81fdd0718f35e1d8e20b
Ludvig <l.vigerust@gmail.com>
2023-08-03 23:12:28 +0200

    Hide Breadcrumbs on error

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -5,42 +5,46 @@
 	$: route = $page.route.id
 </script>
 
-<div class="breadcrumbs flex justify-center py-8 text-sm font-medium">
-	<ul>
-		{#if $page.url.pathname !== '/'}
-			<li><a href="/">Home</a></li>
-		{/if}
+{#if !$page.error}
+	<div class="breadcrumbs flex justify-center py-8 text-sm font-medium">
+		<ul>
+			{#if $page.url.pathname !== '/'}
+				<li><a href="/">Home</a></li>
+			{/if}
 
-		{#if route?.includes('movie')}
-			<li><a href="/movie">Movies</a></li>
+			{#if route?.includes('movie')}
+				<li><a href="/movie">Movies</a></li>
 
-			{#if $page.data.movieDetails}
-				{#if $page.data.movieDetails.belongs_to_collection}
+				{#if $page.data.movieDetails}
+					{#if $page.data.movieDetails.belongs_to_collection}
+						<li>
+							<a
+								href={`/collection/${$page.data.movieDetails.belongs_to_collection.id}-${slugify(
+									$page.data.movieDetails.belongs_to_collection.name
+								)}`}>{$page.data.movieDetails.belongs_to_collection.name}</a
+							>
+						</li>
+					{/if}
+
 					<li>
 						<a
-							href={`/collection/${$page.data.movieDetails.belongs_to_collection.id}-${slugify(
-								$page.data.movieDetails.belongs_to_collection.name
-							)}`}>{$page.data.movieDetails.belongs_to_collection.name}</a
+							href={`/movie/${$page.data.movieDetails.id}-${slugify(
+								$page.data.movieDetails.title
+							)}`}>{$page.data.movieDetails.title}</a
 						>
 					</li>
 				{/if}
+			{:else if route?.includes('tv')}
+				<li><a href="/tv">TV Shows</a></li>
 
-				<li>
-					<a href={`/movie/${$page.data.movieDetails.id}-${slugify($page.data.movieDetails.title)}`}
-						>{$page.data.movieDetails.title}</a
-					>
-				</li>
+				{#if $page.data.showDetails}
+					<li>
+						<a href={`/tv/${$page.data.showDetails.id}-${slugify($page.data.showDetails.name)}`}
+							>{$page.data.showDetails.name}</a
+						>
+					</li>
+				{/if}
 			{/if}
-		{:else if route?.includes('tv')}
-			<li><a href="/tv">TV Shows</a></li>
-
-			{#if $page.data.showDetails}
-				<li>
-					<a href={`/tv/${$page.data.showDetails.id}-${slugify($page.data.showDetails.name)}`}
-						>{$page.data.showDetails.name}</a
-					>
-				</li>
-			{/if}
-		{/if}
-	</ul>
-</div>
+		</ul>
+	</div>
+{/if}
