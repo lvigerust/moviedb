fc6a70aec8b2099f6248ec38068eb280ba71a0a2
Ludvig <ludvig.vigerust@evidi.com>
2023-08-17 00:35:10 +0200

    Changes to meta component

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Movie, PersonDetails, Show } from '$types'
+	import { page } from '$app/stores'
 
 	export let data: Movie | Show | PersonDetails
 
@@ -11,25 +12,34 @@
 
 	function poster(data: Movie | Show | PersonDetails) {
 		if ('poster_path' in data) {
-			return data.poster_path
+			return `https://image.tmdb.org/t/p/w342/${data.poster_path}`
 		} else if ('profile_path' in data) {
-			return data.profile_path
+			return `https://image.tmdb.org/t/p/w342/${data.profile_path}`
 		} else return undefined
 	}
 
 	function backdrop(data: Movie | Show | PersonDetails) {
 		if ('backdrop_path' in data) {
-			return data.backdrop_path
+			return `https://image.tmdb.org/t/p/w780/${data.backdrop_path}`
 		} else return undefined
+	}
+
+	function title(data: Movie | Show | PersonDetails) {
+		if ('title' in data) {
+			return data.title
+		} else return data.name
 	}
 </script>
 
 <svelte:head>
+	<title>{title(data)}</title>
+	<meta property="og:title" content={title(data)} />
+	<meta property="og:type" content="video.movie" />
+	<meta property="og:url" content={$page.data.url} />
 	<meta property="og:description" content={description(data)} />
 
-	<meta property="og:image" content={'https://image.tmdb.org/t/p/w342/' + poster(data)} />
-	<meta property="og:image" content={'https://image.tmdb.org/t/p/w780/' + backdrop(data)} />
+	<meta property="og:image" content={poster(data)} />
+	<meta property="og:image" content={backdrop(data)} />
 
-	<meta property="og:type" content="video.movie" />
 	<meta property="og:site_name" content="Filmguide" />
 </svelte:head>
