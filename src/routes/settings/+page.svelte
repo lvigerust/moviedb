b709b906d9d5e2c4faf0a440ea0035e623c5b62f
Ludvig <ludvig.vigerust@evidi.com>
2023-08-20 21:45:51 +0200

    Updated settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,20 +2,22 @@
 	import { enhance } from '$app/forms'
 	import type { SubmitFunction } from './$types'
 	import Checkbox from './components/Checkbox.svelte'
-	import RadioGroup from './components/RadioGroup.svelte'
 
 	const submitUpdateTheme: SubmitFunction = ({ action }) => {
 		const theme = action.searchParams.get('theme')
 
 		if (theme) {
 			document.documentElement.setAttribute('data-theme', theme)
+			activeTheme = theme
 		}
 	}
+
+	$: activeTheme = document.documentElement.getAttribute('data-theme')
 </script>
 
 <hgroup>
-	<h2>General settings</h2>
-	<p>Customize how the app looks and behaves.</p>
+	<h2>Interface settings</h2>
+	<p>Customize your application theme.</p>
 </hgroup>
 
 <!-- <div class="not-prose flex gap-6">
@@ -23,117 +25,53 @@
 	<RadioGroup />
 </div> -->
 
-<form method="POST" use:enhance={submitUpdateTheme} class="grid h-[270px] grid-cols-3 gap-x-6">
+<form method="POST" use:enhance={submitUpdateTheme} class="grid grid-cols-3 gap-x-6">
 	<button
 		formaction="/settings?/setTheme&theme=auto"
 		type="submit"
-		class="flex h-full w-full flex-col items-end overflow-hidden rounded-lg border border-blue-700"
+		class="flex h-full w-full flex-col items-end overflow-hidden rounded-lg border transition-colors {activeTheme ===
+		'auto'
+			? ' border-blue-700/75'
+			: 'border-slate-700/50'} bg-slate-800/40"
 	>
-		<div class="h-full w-full bg-slate-800/40 px-12 py-6">
-			<div class="flex h-full w-full gap-x-4 rounded-md bg-slate-900 p-4">
-				<div class="flex h-full w-1/3 flex-col justify-between">
-					<div class="space-y-[10px]">
-						<div class="h-4 w-full rounded bg-blue-700" />
-						<div class="h-4 w-full rounded bg-slate-800" />
-						<div class="h-4 w-full rounded bg-slate-800" />
-						<div class="h-4 w-full rounded bg-slate-800" />
-					</div>
-					<div class="h-4 w-full rounded bg-slate-800" />
-				</div>
-
-				<div class="flex h-full w-full flex-col space-y-[10px]">
-					<div class="flex justify-between">
-						<div class="h-4 w-[45%] rounded bg-slate-800" />
-						<div class="flex h-4 w-1/4 gap-x-[6px]">
-							<div class="h-full w-full rounded bg-slate-800" />
-							<div class="h-full w-full rounded bg-blue-700" />
-						</div>
-					</div>
-
-					<div class="h-full w-full rounded-xl bg-slate-800 p-1">
-						<div class="h-full w-full rounded-lg bg-slate-900/80" />
-					</div>
-				</div>
-			</div>
+		<div class="h-full w-full object-contain px-12">
+			<img src="/themes/auto.png" alt="" />
 		</div>
 
 		<div class="w-full bg-slate-900">
-			<p class="my-0 px-10 py-5 text-start font-semibold text-slate-300">System settings</p>
+			<p class="my-0 px-10 py-5 text-start font-semibold text-slate-300">System</p>
 		</div>
 	</button>
 	<button
 		formaction="/settings?/setTheme&theme=light"
 		type="submit"
-		class="flex h-full w-full flex-col items-end overflow-hidden rounded-lg border border-slate-700/50"
+		class="flex h-full w-full flex-col items-end overflow-hidden rounded-lg border transition-colors {activeTheme ===
+		'light'
+			? ' border-blue-700/75'
+			: 'border-slate-700/50'} bg-slate-800/40"
 	>
-		<div class="h-full w-full bg-slate-800/40 px-12 py-6">
-			<div class="flex h-full w-full gap-x-4 rounded-md bg-slate-300 p-4">
-				<div class="flex h-full w-1/3 flex-col justify-between">
-					<div class="space-y-[10px]">
-						<div class="h-4 w-full rounded bg-blue-700" />
-						<div class="h-4 w-full rounded bg-slate-400" />
-						<div class="h-4 w-full rounded bg-slate-400" />
-						<div class="h-4 w-full rounded bg-slate-400" />
-					</div>
-					<div class="h-4 w-full rounded bg-slate-400" />
-				</div>
-
-				<div class="flex h-full w-full flex-col space-y-[10px]">
-					<div class="flex justify-between">
-						<div class="h-4 w-[45%] rounded bg-slate-400" />
-						<div class="flex h-4 w-1/4 gap-x-[6px]">
-							<div class="h-full w-full rounded bg-slate-400" />
-							<div class="h-full w-full rounded bg-blue-700" />
-						</div>
-					</div>
-
-					<div class="h-full w-full rounded-xl bg-slate-400 p-1">
-						<div class="h-full w-full rounded-lg bg-slate-300/90" />
-					</div>
-				</div>
-			</div>
+		<div class="h-full w-full object-contain px-12">
+			<img src="/themes/light.png" alt="" />
 		</div>
 
 		<div class="w-full bg-slate-900">
-			<p class="my-0 px-10 py-5 text-start font-semibold text-slate-300">Light theme</p>
+			<p class="my-0 px-10 py-5 text-start font-semibold text-slate-300">Light</p>
 		</div>
 	</button>
-
 	<button
 		formaction="/settings?/setTheme&theme=night"
 		type="submit"
-		class="flex h-full w-full flex-col items-end overflow-hidden rounded-lg border border-slate-700/50"
+		class="flex h-full w-full flex-col items-end overflow-hidden rounded-lg border transition-colors {activeTheme ===
+		'night'
+			? ' border-blue-700/75'
+			: 'border-slate-700/50'} bg-slate-800/40"
 	>
-		<div class="h-full w-full bg-slate-800/40 px-12 py-6">
-			<div class="flex h-full w-full gap-x-4 rounded-md bg-slate-900 p-4">
-				<div class="flex h-full w-1/3 flex-col justify-between">
-					<div class="space-y-[10px]">
-						<div class="h-4 w-full rounded bg-blue-700" />
-						<div class="h-4 w-full rounded bg-slate-800" />
-						<div class="h-4 w-full rounded bg-slate-800" />
-						<div class="h-4 w-full rounded bg-slate-800" />
-					</div>
-					<div class="h-4 w-full rounded bg-slate-800" />
-				</div>
-
-				<div class="flex h-full w-full flex-col space-y-[10px]">
-					<div class="flex justify-between">
-						<div class="h-4 w-[45%] rounded bg-slate-800" />
-						<div class="flex h-4 w-1/4 gap-x-[6px]">
-							<div class="h-full w-full rounded bg-slate-800" />
-							<div class="h-full w-full rounded bg-blue-700" />
-						</div>
-					</div>
-
-					<div class="h-full w-full rounded-xl bg-slate-800 p-1">
-						<div class="h-full w-full rounded-lg bg-slate-900/80" />
-					</div>
-				</div>
-			</div>
+		<div class="h-full w-full object-contain px-12">
+			<img src="/themes/dark.png" alt="" />
 		</div>
 
 		<div class="w-full bg-slate-900">
-			<p class="my-0 px-10 py-5 text-start font-semibold text-slate-300">Dark theme</p>
+			<p class="my-0 px-10 py-5 text-start font-semibold text-slate-300">Dark</p>
 		</div>
 	</button>
 </form>
